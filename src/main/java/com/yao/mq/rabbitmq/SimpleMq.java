9c7c0ff3983751454ac32458df09660c463e1fcc
yaojian <1161995598@qq.com>
2022-01-13 15:57:17 +0800

    Initial commit

---
+++ src/main/java/com/yao/mq/rabbitmq/SimpleMq.java
@@ -0,0 +1,16 @@
+package com.yao.mq.rabbitmq;
+
+/**
+ * Created by yaojian on 2022/1/13 15:34
+ *
+ * @author
+ */
+public class SimpleMq {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
